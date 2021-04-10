425016d5fd49677e632af4d2524ce4ec0946733e
ekaji <ekajionah@gmail.com>
2021-04-10 14:20:18 +0100

    added hamburger menu and javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+function myFunction(){
+    let icon = document.querySelector(".icon");
+    let menuList = document.querySelector(".icon ~ div.menu-background");
+    menuList.classList.toggle('menu-hidden')
+}
